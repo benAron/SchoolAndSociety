bb20e09f08dc0595d5a42ab7e2624cea7e99e66c
benAron <blaron@mit.edu>
2015-04-16 18:58:55 -0400

    for loop

--- Rate.java
+++ Rate.java
@@ -4,10 +4,11 @@ public class Rate {
   int scope; //how many words per cycle
   int altered; //how many words translated per cycle
   int inputAmount; // total amount of words in input
+  String wordForTranslate = "";
   
   Random rand = new Random(); 
   
-  public String cycle (int currentScope, int currentAlt, ArrayList<String> fullInput) { //uses translate.java to produce translation
+  public String cycle (int currentScope, int currentAlt, ArrayList<String> fullSearch, ArrayList<String> fullInput) { //use Translations.java to produce translation
     scope = currentScope;
     altered = currentAlt; 
     inputAmount = fullInput.size();
@@ -24,9 +25,16 @@ public class Rate {
     }
     System.out.println("Random numbers generated.");
     
+    //Cycle through segment of book and put through translate
+    boolean translateOption = true;
     
-    
-    
-    
-  }
+    for(int i = 0; inputAmount > i; i++) { 
+      wordForTranslate = fullInput.get(i); //pick word from input
+      
+      if(fullSearch.contains(wordForTranslate) != true) {
+        translateOption = false; 
+      }
+      
+      jargon.translate(i, translateOption);
+    }
 }
